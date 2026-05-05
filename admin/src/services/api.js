eac1961945bffc82b19056a2790110d239b3fff7
Ashif-Ewp <ashif.ewp@gmail.com>
2026-05-05 13:55:27 +0530

    add up comm!

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -2,15 +2,16 @@ import axios from "axios";
 import { TOKEN_KEY } from "../utils/constants";
 
 const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
+// TODO: We should probably split this into two instances, one for the admin and one for the client, so that we don't have to check the URL in the response interceptor. But for now, we'll just use one instance and check the URL in the response interceptor.
 const api = axios.create({
   baseURL: baseUrl,
   withCredentials: true,
 });
 
 function readCsrfCookie() {
-  if (typeof document === 'undefined') return '';
+  if (typeof document === "undefined") return "";
   const m = document.cookie.match(/(?:^|;\s*)markup_csrf=([^;]+)/);
-  return m ? decodeURIComponent(m[1]) : '';
+  return m ? decodeURIComponent(m[1]) : "";
 }
 
 api.interceptors.request.use((config) => {
@@ -20,7 +21,7 @@ api.interceptors.request.use((config) => {
   }
   const csrf = readCsrfCookie();
   if (csrf) {
-    config.headers['X-CSRF-Token'] = csrf;
+    config.headers["X-CSRF-Token"] = csrf;
   }
   return config;
 });
